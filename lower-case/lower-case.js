fd60e00528ffe44c34d86757e13a6f69a3ca15cf
laggers <stefan.lagger@roche.com>
2019-04-17 12:43:36 +0200

    Anderer Text

--- lower-case/lower-case.js
+++ lower-case/lower-case.js
@@ -3,7 +3,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase() + "bye";
+            msg.payload = msg.payload.toLowerCase() + "how're you?";
             node.send(msg);
         });
     }
