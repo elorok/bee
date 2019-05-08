d070c45fcc53fe48c04a4f09433ccbe34b9bbe7b
laggerstefan <stefan.lagger@roche.com>
2019-05-08 10:04:58 +0200

    Cyclic Button Call

--- button/button.js
+++ button/button.js
@@ -1,13 +1,27 @@
 module.exports = function (RED) {
     "use strict";
 
+    var pollState = function () {
+        var address = 10; 
+
+        var exec = require('child_process').exec;
+        exec('/home/pi/bee/_tools/i2c/readByte.py ' + address, function callback(error, stdout, stderr) {
+            msg.payload = stdout;
+            node.send(msg);
+        });
+
+        timeout = setTimeout(pollState, this.readFrequency);
+    }
+
     function ButtonNode(config) {
         RED.nodes.createNode(this, config);
 
         this.readFrequency = config.readFrequency; 
         var node = this;
 
-        node.on('input', function (msg) {
+        this.timeout = setTimeout(pollState, this.readFrequency);
+
+        /*node.on('input', function (msg) {
             var address = 10;
 
             var exec = require('child_process').exec;
@@ -15,7 +29,7 @@ module.exports = function (RED) {
                 msg.payload = stdout;
                 node.send(msg);
             });
-        });
+        });*/
     }
     RED.nodes.registerType("button", ButtonNode);
 }
