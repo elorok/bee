c59083e0082c33b8a7a6c55afa825a8160ac1763
laggerstefan <stefan.lagger@roche.com>
2019-05-08 13:19:53 +0200

    Removed Freq

--- button/button.js
+++ button/button.js
@@ -1,27 +1,12 @@
 module.exports = function (RED) {
     "use strict";
 
-    var pollState = function () {
-        var address = 10; 
-
-        var exec = require('child_process').exec;
-        exec('/home/pi/bee/_tools/i2c/readByte.py ' + address, function callback(error, stdout, stderr) {
-            msg.payload = stdout;
-            node.send(msg);
-        });
-
-        timeout = setTimeout(pollState, this.readFrequency);
-    }
-
     function ButtonNode(config) {
         RED.nodes.createNode(this, config);
 
-        this.readFrequency = config.readFrequency; 
         var node = this;
 
-        this.timeout = setTimeout(pollState, this.readFrequency);
-
-        /*node.on('input', function (msg) {
+        node.on('input', function (msg) {
             var address = 10;
 
             var exec = require('child_process').exec;
@@ -29,7 +14,7 @@ module.exports = function (RED) {
                 msg.payload = stdout;
                 node.send(msg);
             });
-        });*/
+        });
     }
     RED.nodes.registerType("button", ButtonNode);
 }
