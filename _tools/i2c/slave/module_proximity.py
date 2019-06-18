f6e36f98a3372f87e0af1f58e3ed020b68596243
laggerstefan <stefan.lagger@roche.com>
2019-06-18 14:36:07 +0200

    Proximity

--- _tools/i2c/slave/module_proximity.py
+++ _tools/i2c/slave/module_proximity.py
@@ -4,11 +4,17 @@ import sys
 class Proximity:
 		def __init__(self):
 			self.ADDR = 81
+			i2c = smbus.SMBus(1)
+			i2c.write_word_data(self.ADDR, 0, 0)	# Enable Ambilight Sensor
+			i2c.write_word_data(self.ADDR, 3, 2250)	# Setup Proximity Sensor
+			i2c.write_word_data(self.ADDR, 4, 1888) #
+
 
 		def sync(self):
 			i2c = smbus.SMBus(1)
-			result = i2c.read_word_data(self.ADDR, 8)
+			proxy = i2c.read_word_data(self.ADDR, 8)
+			proxy = i2c.read_word_data(self.ADDR, 9)
 
 			file = open("/tmp/i2c_" + str(self.ADDR), "w")
-			file.write(str(result))
+			file.write(str(proxy) + "\n" + str(ambi))
 			file.close()
