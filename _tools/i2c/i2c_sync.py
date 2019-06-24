968bea5476fff13c360f7040c9fe1298e3ff50da
laggerstefan <stefan.lagger@roche.com>
2019-06-24 11:22:24 +0200

    Time measurement

--- _tools/i2c/i2c_sync.py
+++ _tools/i2c/i2c_sync.py
@@ -14,27 +14,31 @@ modules.append(Led())
 
 GPIO.setmode(GPIO.BCM)
 
+tBusBlocked = 0
+
 while(True):
+	if(GPIO.input(2) and GPIO.input(3)):	# I2C bus free
+		tBusBlocked = 0
 
-	print("SDA: " + str(GPIO.input(2)))
-	print("SCL: " + str(GPIO.input(3)))
+		for module in modules:
+			# Module is offline
+			if not module.getOnline():
+				module.checkOnline()
+				module.setSetup(False)
 
-	for module in modules:
-		# Module is offline
-		if not module.getOnline():
-			module.checkOnline()
-			module.setSetup(False)
+			# Module is not initialized
+			if module.getOnline() and not module.getSetup():
+				try:
+					module.setup()
+				except: 
+					pass
 
-		# Module is not initialized
-		if module.getOnline() and not module.getSetup():
-			try:
-				module.setup()
-			except: 
-				pass
+			# Module is ready
+			if module.getOnline() and module.getSetup():
+				module.sync()
 
-		# Module is ready
-		if module.getOnline() and module.getSetup():
-			module.sync()
-
+	else:	# I2C bus blocked
+		tBusBlocked++
+		print("Blocked: " + str(tBusBlocked * 100) + "ms")
 
 	sleep(0.1)	# 100ms
