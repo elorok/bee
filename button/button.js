5f6e8f5889cc24078bb72c146578206136547368
laggerstefan <stefan.lagger@roche.com>
2019-06-24 13:07:58 +0200

    Setup

--- button/button.js
+++ button/button.js
@@ -9,17 +9,38 @@ module.exports = function (RED) {
         node.on('input', function (msg) {
             var fs = require('fs');
 
-            fs.readFile('/tmp/i2c_10', 'utf8', function (error, contents) {
+            try {
+                fs.readFile('/tmp/i2c_10', 'utf8', function (error, contents) {
+                    if (error == null) {
+                        node.status({ fill: "green", shape: "dot", text: "connected" });
+                        msg.payload = contents;
+                        node.send(msg);
+                    }
+                    else {
+                        node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                        node.error(error);
+                    }
+                })
+            } catch (error) {
+                node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                node.message(error);
+            }
+
+
+            /*var address = 10;
+
+            var exec = require('child_process').exec;
+            exec('/home/pi/bee/_tools/i2c/readByte.py ' + address, function callback(error, stdout, stderr) {
                 if (error == null) {
                     node.status({ fill: "green", shape: "dot", text: "connected" });
-                    msg.payload = contents;
+                    msg.payload = stdout;
                     node.send(msg);
                 }
                 else {
                     node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                    node.message(error);
+                    node.error(stderr);
                 }
-            })
+            });*/
         });
     }
     RED.nodes.registerType("button", ButtonNode);
