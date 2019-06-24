ab34ad30c922c41805ee58ddd27afdd3fafaa9f0
laggerstefan <stefan.lagger@roche.com>
2019-06-24 12:17:58 +0200

    Setup

--- _tools/i2c/i2c_sync.py
+++ _tools/i2c/i2c_sync.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from time import sleep
-import RPi.GPIO as GPIO
 from slave.module_button import Button
 from slave.module_proximity import Proximity
 from slave.module_led import Led
@@ -12,35 +11,23 @@ modules.append(Button())
 modules.append(Proximity())
 modules.append(Led())
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.IN)	# SDA
-GPIO.setup(3, GPIO.IN)	# SCL
-
-tBusBlocked = 0
 
 while(True):
-	if(GPIO.input(2) and GPIO.input(3)):	# I2C bus free
-		tBusBlocked = 0
+	for module in modules:
+		# Module is offline
+		if not module.getOnline():
+			module.checkOnline()
+			module.setSetup(False)
 
-		for module in modules:
-			# Module is offline
-			if not module.getOnline():
-				module.checkOnline()
-				module.setSetup(False)
+		# Module is not initialized
+		if module.getOnline() and not module.getSetup():
+			try:
+				module.setup()
+			except: 
+				pass
 
-			# Module is not initialized
-			if module.getOnline() and not module.getSetup():
-				try:
-					module.setup()
-				except: 
-					pass
-
-			# Module is ready
-			if module.getOnline() and module.getSetup():
-				module.sync()
-
-	else:	# I2C bus blocked
-		tBusBlocked += 1
-		print("Blocked: " + str(tBusBlocked * 100) + "ms")
+		# Module is ready
+		if module.getOnline() and module.getSetup():
+			module.sync()
 
 	sleep(0.1)	# 100ms
