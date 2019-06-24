c0bce0e44a50a692eb2ee2ed1e3dc23d73682b8f
laggerstefan <stefan.lagger@roche.com>
2019-06-24 13:26:00 +0200

    Setup

--- proximity_ambilight/proximity.js
+++ proximity_ambilight/proximity.js
@@ -9,12 +9,30 @@ module.exports = function (RED) {
         var address = 81;
         var exec = require('child_process').exec;
 
-        // Init Sensor
-        exec('/home/pi/bee/_tools/i2c/writeCommandWord.py ' + address + ' 0 0');    // Enable Ambilight Sensor
-        exec('/home/pi/bee/_tools/i2c/writeCommandWord.py ' + address + ' 3 2250');    // Setup Proximity Sensor
-        exec('/home/pi/bee/_tools/i2c/writeCommandWord.py ' + address + ' 4 1888');    //
 
         node.on('input', function (msg) {
+            var fs = require('fs');
+
+            try {
+                fs.readFile('/tmp/i2c_81', 'utf8', function (error, contents) {
+                    if (error == null) {
+                        if (contents != "<offline>") {
+                            node.status({ fill: "green", shape: "dot", text: "connected" });
+                            msg.payload = contents; 
+                            node.send([msg, null]);
+                            msg.payload = contents;
+                            node.send([null, msg]);
+                        } else {
+                            node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                        }
+                    } else {
+                        node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                        node.error(stderr);
+                    }
+                })
+            }
+
+            /*
             exec('/home/pi/bee/_tools/i2c/readCommandWord.py ' + address + ' 8', function callback(error, stdout, stderr) {   // Read Register Proximity
                 if (error == null) {
                     node.status({ fill: "green", shape: "dot", text: "connected" });
@@ -32,7 +50,7 @@ module.exports = function (RED) {
                     msg.payload = stdout;
                     node.send([null,msg]);
                 }
-            });
+            });*/
         });
     }
     RED.nodes.registerType("proximity", ProximityNode);
