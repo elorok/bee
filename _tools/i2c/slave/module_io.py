634471ffe457c4e71d0b78a17ab4f9d294f6246b
gehrigm2 <gehrigm2@TROMW031757>
2019-10-11 11:04:05 +0200

    Module IO

--- _tools/i2c/slave/module_io.py
+++ _tools/i2c/slave/module_io.py
@@ -16,6 +16,7 @@ class Io(Module):
 				try: 
 					i2c = smbus.SMBus(1)
 					state = i2c.read_word_data(super().getAddress(), 1)
+					state = state + i2c_rea_byte(supeer().getAddress)
 					file = open("/tmp/i2c_" + str(super().getAddress()) + "_in", "w")
 					file.write(str(state))
 					file.close()
