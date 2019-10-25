4e198ecfdc2872a8c5b394de6207b180d76584d8
laggers <stefan.lagger@roche.com>
2019-10-25 14:37:24 +0200

    Bugfix für die neue Node-Red Version

--- proximity_ambilight/proximity.js
+++ proximity_ambilight/proximity.js
@@ -19,10 +19,9 @@ module.exports = function (RED) {
                         } else {
                             node.status({ fill: "green", shape: "dot", text: "connected" });
 
-                            msg.payload = contents.split("\n")[0];
-                            node.send([msg, null]);
-                            msg.payload = contents.split("\n")[1];
-                            node.send([null, msg]);
+                            var msgP = { payload: contents.split("\n")[0] };    // Proximity
+                            var msgA = { payload: contents.split("\n")[1] };    // Ambilight
+                            node.send([msgP, msgA]);
                         }
                     } else {
                         node.status({ fill: "red", shape: "dot", text: "disconnected" });
