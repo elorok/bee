8cd1cb066060a1bf44576de23a72fa31fe9968a0
laggers <stefan.lagger@roche.com>
2020-01-20 11:11:33 +0100

    Add module servo

---
+++ servo/servo.js
@@ -0,0 +1,46 @@
+module.exports = function (RED) {
+    "use strict";
+
+    function ServoNode(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+
+        node.on('input', function (msg) {
+            var fs = require('fs');
+
+            // *** Read State ***
+            try {
+                fs.readFile('/tmp/i2c_11_in', 'utf8', function (error, contents) {
+                    if (error == null) {
+                        if (contents != "<offline>") {
+                            node.status({ fill: "green", shape: "dot", text: "connected" });
+                        } else {
+                            node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                        }
+                    } else {
+                        node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                        node.error(stderr);
+                    }
+                })
+            } catch (error) {
+                node.status({ fill: "red", shape: "dot", text: "disconnected" });
+                node.error(error);
+            }
+
+
+            // *** Write Data ***
+            try {
+                var pwm = parseInt(msg.payload); 
+
+
+                fs.writeFile('/tmp/i2c_11_out', pwm.toString(10), function (error) {
+                    if (error) throw error;
+                })
+            } catch (error) {
+                node.error(error);
+            }
+        });
+    }
+    RED.nodes.registerType("servo", ServoNode);
+}
