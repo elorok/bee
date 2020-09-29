cf777d823f21bcb8a3e86315e364f92abf728c27
rufg <68429117+rufg@users.noreply.github.com>
2020-09-29 13:50:22 +0200

    Update servo.js
    Changed from adress "11" to "12"

--- servo/servo.js
+++ servo/servo.js
@@ -11,7 +11,7 @@ module.exports = function (RED) {
 
             // *** Read State ***
             try {
-                fs.readFile('/tmp/i2c_11_in', 'utf8', function (error, contents) {
+                fs.readFile('/tmp/i2c_12_in', 'utf8', function (error, contents) {
                     if (error == null) {
                         if (contents != "<offline>") {
                             node.status({ fill: "green", shape: "dot", text: "connected" });
@@ -34,7 +34,7 @@ module.exports = function (RED) {
                 var pwm = parseInt(msg.payload); 
 
 
-                fs.writeFile('/tmp/i2c_11_out', pwm.toString(10), function (error) {
+                fs.writeFile('/tmp/i2c_12_out', pwm.toString(10), function (error) {
                     if (error) throw error;
                 })
             } catch (error) {
