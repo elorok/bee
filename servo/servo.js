80ddc82b41c2165bae3dfc85d692dc39a247f550
rufg <68429117+rufg@users.noreply.github.com>
2020-10-05 12:22:49 +0200

    Update servo.js
    Write on the correct line

--- servo/servo.js
+++ servo/servo.js
@@ -38,11 +38,36 @@ module.exports = function (RED) {
                 })
                 
                 //Test Gianni 30-SEP-2020
-                var parts = msg.payload.split(",");
-                parts[1] = parseInt(parts[1]);
-                fs.writeFile('/tmp/testing', parts[0].toString(10) + "\n" + parts[1].toString(10), function (error) {
-                    if (error) throw error;
-                })
+                var parts = msg.payload.split(","); //split @comma
+                parts[1] = parseInt(parts[1], 16);
+                
+                a_file = open('/tmp/testing', 'r');
+                lines = a_file.readlines();
+                a_file.close();
+                
+                del lines[(parts[0]-1)]
+                
+                new_file = open('/tmp/testing', 'w+');
+                for line in lines:
+                    new_file.write(line);
+                
+                new_file.close();
+                
+                var line = 0;
+                var char = 0;
+                
+                file = open('/tmp/testing', 'w');
+                while line <= 6:
+                    if line == parts[0]:
+                        file.write(parts[1].toString(10) + "\n");
+                        break;
+                    char = file.read(1);
+                    if char == '\n':
+                        line = line + 1;
+                
+//              only for testing  fs.writeFile('/tmp/testing', parts[0].toString(10) + "\n" + parts[1].toString(10), function (error) {
+//                  if (error) throw error;
+//              })
                 
             } catch (error) {
                 node.error(error);
