5a16863c32ce60e28156d8e045f466a6f066b9da
rufg <68429117+rufg@users.noreply.github.com>
2020-10-05 13:36:53 +0200

    Update servo.js

--- servo/servo.js
+++ servo/servo.js
@@ -40,32 +40,7 @@ module.exports = function (RED) {
                 //Test Gianni 30-SEP-2020
                 var parts = msg.payload.split(","); //split at comma
                 parts[1] = parseInt(parts[1], 16);
-                
-                /*a_file = open('/tmp/testing', 'r');
-                lines = a_file.readlines();
-                a_file.close();
-                
-                del lines[(parts[0]-1)]
-                
-                new_file = open('/tmp/testing', 'w+');
-                for line in lines:
-                    new_file.write(line);
-                
-                new_file.close();
-                
-                var line = 0;
-                var char = 0;
-                
-                file = open('/tmp/testing', 'w');
-                while line <= 6:
-                    if line == parts[0]:
-                        file.write(parts[1].toString(10) + "\n");
-                        break;
-                    char = file.read(1);
-                    if char == '\n':
-                        line = line + 1;
-                */
-                //for Testing
+
                 fs.writeFile('/tmp/i2c_12_out', parts[0].toString(10) + "\n" + parts[1].toString(10), function (error) {
                     if (error) throw error;
                 })
