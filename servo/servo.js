70062f0d174e56514cef869d296df49d946d9029
rufg <68429117+rufg@users.noreply.github.com>
2020-10-06 10:06:53 +0200

    Update servo.js
    Change to correct Java

--- servo/servo.js
+++ servo/servo.js
@@ -30,44 +30,26 @@ module.exports = function (RED) {
 
 
             // *** Write Data ***
-            try {            
-                with open("/tmp/i2c_12_out", "r") as read_obj:
-                    try{
-                        var one_char = read_obj.read(1);
-                    read_obj.close();
-                    }
-                    catch (error) {
-                        node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                        node.error(error);
-                    }
+            //check if file is empty
+            outFile = 
+            if ('/tmp/i2c_12_out'.length() == 0){
                 var zahlNull = "0";
-                if (not one_char) {
-                    fs.writeFile("/tmp/testing", zahlNull + "\n" + zahlNull + "\n" + zahlNull + "\n" + zahlNull + "\n" + zahlNull + "\n" +zahlNull + "\n", function (error){
-                        if (error) throw error;
-                    })
-                }
-                catch (error) {
-                    node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                    node.error(error);
-                }
-                
-                var pwm = parseInt(msg.payload); 
-
-                fs.writeFile("/tmp/testing", pwm.toString(10), function (error) {
+                fs.writeFile('/tmp/testing', zahlNull + "\n" + zahlNull + "\n" + zahlNull + "\n" + zahlNull + "\n" + zahlNull + "\n" +zahlNull + "\n", function (error){
                     if (error) throw error;
                 })
-                
-                //Test Gianni 30-SEP-2020
-                var parts = msg.payload.split(","); //split at comma
-                parts[1] = parseInt(parts[1], 16);
-
-                fs.writeFile("/tmp/i2c_12_out", parts[0].toString(10) + "\n" + parts[1].toString(10), function (error) {
-                    if (error) throw error;
-                })
-                
-            } catch (error) {
-                node.error(error);
             }
+            var pwm = parseInt(msg.payload); 
+
+            fs.writeFile('/tmp/testing', pwm.toString(10), function (error) {
+                if (error) throw error;
+            })
+
+            var parts = msg.payload.split(","); //split at comma
+            parts[1] = parseInt(parts[1], 16);
+
+            fs.writeFile('/tmp/i2c_12_out', parts[0].toString(10) + "\n" + parts[1].toString(10), function (error) {
+                if (error) throw error;
+            })
         });
     }
     RED.nodes.registerType("servo", ServoNode);
