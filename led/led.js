52b41572cae8ad44a60fc98b7212724e140c3c86
rufg <68429117+rufg@users.noreply.github.com>
2020-11-11 15:17:41 +0100

    Update led.js
    changes: work with fd, add finally{}

--- led/led.js
+++ led/led.js
@@ -11,7 +11,9 @@ module.exports = function (RED) {
 
             // *** Read State ***
             try {
-                fs.readFile('/tmp/i2c_9_in', 'utf8', function (error, contents) {
+                var fd_in;  //filedescripter inputFile
+                fd_in = fs.openSync('/tmp/i2c_9_in', 'r');
+                fs.readFile(fd_in, 'utf8', function (error, contents) {
                     if (error == null) {
                         if (contents != "<offline>") {
                             node.status({ fill: "green", shape: "dot", text: "connected" });
@@ -26,21 +28,30 @@ module.exports = function (RED) {
             } catch (error) {
                 node.status({ fill: "red", shape: "dot", text: "disconnected" });
                 node.error(error);
+            }finally{
+                if(fd_in){
+                    fs.closeSync(fd_in);
+                }
             }
 
 
             // *** Write Data ***
             try {
+                var fd_out;  //filedescripter outputFile
                 var red = parseInt(msg.payload.substring(0, 2), 16);
                 var green = parseInt(msg.payload.substring(2, 4), 16);
                 var blue = parseInt(msg.payload.substring(4, 6), 16);
 
-
-                fs.writeFile('/tmp/i2c_9_out', red.toString(10) + "\n" + green.toString(10) + "\n" + blue.toString(10), function (error) {
+                fd_out = fs.openSync('/tmp/i2c_9_out', 'w');
+                fs.writeFile(fd_out, red.toString(10) + "\n" + green.toString(10) + "\n" + blue.toString(10), function (error) {
                     if (error) throw error;
                 })
             } catch (error) {
                 node.error(error);
+            }finally{
+                if(fd_out){
+                    fs.closeSync(fd_out);
+                }
             }
         });
     }
