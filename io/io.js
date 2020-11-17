3f335d2cb987457a0a74782d35e73a0b2a7d28ec
rufg <68429117+rufg@users.noreply.github.com>
2020-11-17 12:40:13 +0100

    Update io.js
    change from async read/write to sync read/write

--- io/io.js
+++ io/io.js
@@ -1,131 +1,109 @@
 module.exports = function (RED) {
-    "use strict";
+	"use strict";
 
-    function IoNode(config) {
-        RED.nodes.createNode(this, config);
+	function IoNode(config) {
+		RED.nodes.createNode(this, config);
 
-        var node = this;
+		var node = this;
 
-        node.on('input', function (msg) {
-            var fs = require('fs');
+		node.on('input', function (msg) {
+			var fs = require('fs');
             
-            // *** Read Data ***
-            try {
-                var fd_in;  //filedescripter inputFile
-                fd_in = fs.openSync('/tmp/i2c_11_in', 'r');
-                fs.readFile(fd_in, 'utf8', function (error, contents) {
-                    if (error == null) {
-                        if (contents == "<offline>") {
-                            node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                        } else if (contents == "<online>") {
-                            node.status({ fill: "green", shape: "dot", text: "connected" });
-                        } else {
-                            node.status({ fill: "green", shape: "dot", text: "connected" });
-                            var msgA = { payload: contents.split("\n")[0] };    // Analog
-                            var msgD = contents.split("\n")[1];                 // digital
-                            var msgD0 = { payload: Math.round(msgD % 2) };
-                            var msgD1 = { payload: Math.round(msgD / 2 % 2) };
-                            var msgD2 = { payload: Math.round(msgD / 4 % 2) };
-                            var msgD3 = { payload: Math.round(msgD / 8 % 2) };
-                            node.send([msgA, msgD0, msgD1, msgD2, msgD3]);
-                        }
-                    }
-                    else {
-                        node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                        node.error(error);
-                    }
-                })
-            } catch (error) {
-                node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                node.error(error);
+			// *** Read State and Data ***
+			var fd_in;  //filedescripter inputFile
+			try {
+				fd_in = fs.openSync('/tmp/i2c_11_in', 'r');
+				var contents = fs.readFileSync(fd_in, 'utf8');                  
+				if (contents == "<offline>") {
+					node.status({ fill: "red", shape: "dot", text: "disconnected" });
+				} else if (contents == "<online>") {
+					node.status({ fill: "green", shape: "dot", text: "connected" });
+				} else {
+					node.status({ fill: "green", shape: "dot", text: "connected" });
+					var msgA = { payload: contents.split("\n")[0] };    // Analog
+					var msgD = contents.split("\n")[1];                 // digital
+					var msgD0 = { payload: Math.round(msgD % 2) };
+					var msgD1 = { payload: Math.round(msgD / 2 % 2) };
+					var msgD2 = { payload: Math.round(msgD / 4 % 2) };
+					var msgD3 = { payload: Math.round(msgD / 8 % 2) };
+					node.send([msgA, msgD0, msgD1, msgD2, msgD3]);
+				}
+			} catch (error) {
+				node.status({ fill: "red", shape: "dot", text: "disconnected" });
+				node.error(error);
             } finally {
-                if(fd_in){
-                    fs.closeSync(fd_in);
-                }
-            }
+				if(fd_in){
+					fs.closeSync(fd_in);
+				}
+			}
 
-            // *** Write Data ***
-            try {
-                var fd_out;  //filedescripter outputFile
-                var payload = msg.payload;
-                var topic = parseInt(msg.topic, 16);
-                var output;
+			// *** Write Data ***
+			var fd_out;  //filedescripter outputFile
+			try {
+				var payload = msg.payload;
+				var topic = parseInt(msg.topic, 16);
+				var output;
 
-                const lockFilePath = "/tmp/.i2c_11_out.lock";
-                while (fs.existsSync(lockFilePath)) {
-                    await sleep(100);
-                }
-                fs.open(lockFilePath, 'w', function (err, file) {
-                    if (err) throw err;
-                });
+				const lockFilePath = "/tmp/.i2c_11_out.lock";
+				while (fs.existsSync(lockFilePath)) {
+                    ;
+				}
+				var fd_lock = fs.openSync(lockFilePath, 'w');
                 
-                fd_out = fs.openSync('/tmp/i2c_11_out', 'a+');
-                fs.readFile(fd_out, 'utf8', function (error, content) {
-                    output = parseInt(content);
-                    if (error) throw error;                    
+				fd_out = fs.openSync('/tmp/i2c_11_out', 'a+');
+				var content = fs.readFileSync(fd_out, 'utf8');
+				output = parseInt(content);
 
-                    switch (topic) {
-                        case 0:
-                            if (payload == true) {
-                                output = (output | 1 << 0);
-                            }
-                            else if (payload == false) {
-                                output = (output & ~(1 << 0));
-                            }
-                            break;
-                        case 1:
-                            if (payload == true) {
-                                output = (output | 1 << 1);
-                            }
-                            else if (payload == false) {
-                                output = (output & ~(1 << 1));
-                            }
-                            break;
-                        case 2:
-                            if (payload == true) {
-                                output = (output | 1 << 2);
-                            }
-                            else if (payload == false) {
-                                output = (output & ~(1 << 2));
-                            }
-                            break;
-                        case 3:
-                            if (payload == true) {
-                                output = (output | 1 << 3);
-                            }
-                            else if (payload == false) {
-                                output = (output & ~(1 << 3));
-                            }
-                            break;
-                        default:
-                            break;
-                    }
+				switch (topic) {
+					case 0:
+						if (payload == true) {
+							output = (output | 1 << 0);
+						}
+						else if (payload == false) {
+							output = (output & ~(1 << 0));
+						}
+						break;
+					case 1:
+						if (payload == true) {
+							output = (output | 1 << 1);
+						}
+						else if (payload == false) {
+							output = (output & ~(1 << 1));
+						}
+						break;
+					case 2:
+						if (payload == true) {
+							output = (output | 1 << 2);
+						}
+						else if (payload == false) {
+							output = (output & ~(1 << 2));
+						}
+						break;
+					case 3:
+						if (payload == true) {
+							output = (output | 1 << 3);
+						}
+						else if (payload == false) {
+							output = (output & ~(1 << 3));
+						}
+						break;
+					default:
+						break;
+				}
 
-                    output = output.toString(10);
-                    if (output != null && output != "") {
-                        fs.writeFile('/tmp/i2c_11_out', output, function (error) {
-                            if (error) throw error;
-                        })
-                    }
-                })
-                fs.unlink(lockFilePath, function (err) {
-                    if (err) throw err;
-                });
-                
-            } catch (error) {
-                node.error(error);
-            } finally {
-                if(fd_out){
-                    fs.closeSync(fd_out);
-                }
-            }
-        });
-    }
-    RED.nodes.registerType("io", IoNode)
-
-    function sleep(ms) {
-        return new Promise((resolve) => {
-            setTimeout(resolve, ms);
-        });
-    }   
+				output = output.toString(10);
+				if (output != null && output != "") {
+					fs.writeFileSync('/tmp/i2c_11_out', output);
+				}
+				fs.unlinkSync(lockFilePath);                
+			} catch (error) {
+				node.error(error);
+			} finally {
+				if(fd_out){
+					fs.closeSync(fd_out);
+				}
+			}
+		});
+	}
+    RED.nodes.registerType("io", IoNode)   
 }
