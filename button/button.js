dd45408def37a3037afd8a77589456f7f31ecf48
rufg <68429117+rufg@users.noreply.github.com>
2020-11-17 11:12:57 +0100

    Update button.js
    change from async read to sync read

--- button/button.js
+++ button/button.js
@@ -1,43 +1,37 @@
 module.exports = function (RED) {
-    "use strict";
+	"use strict";
 
-    function ButtonNode(config) {
-        RED.nodes.createNode(this, config);
+	function ButtonNode(config) {
+		RED.nodes.createNode(this, config);
 
-        var node = this;
+		var node = this;
 
-        node.on('input', function (msg) {
-            var fs = require('fs');
+		node.on('input', function (msg) {
+			var fs = require('fs');
 
-            try {
-                var fd_in;  //filedescripter InputFile
-                fd_in = fs.openSync('/tmp/i2c_10_in', 'r');
-                fs.readFile(fd_in, 'utf8', function (error, contents) {
-                    if (error == null) {
-                        if (contents == "<offline>") {
-                            node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                        } else if (contents == "<online>") {
-                            node.status({ fill: "green", shape: "dot", text: "connected" });
-                        } else {
-                            node.status({ fill: "green", shape: "dot", text: "connected" });
-                            msg.payload = contents;
-                            node.send(msg);
-                        }
-                    }
-                    else {
-                        node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                        node.error(error);
-                    }
-                })
-            } catch (error) {
-                node.status({ fill: "red", shape: "dot", text: "disconnected" });
-                node.error(error);
-            } finally {
-                if(fd_in){
-                    fs.closeSync(fd_in);
-                }
-            }
-        });
-    }
-    RED.nodes.registerType("button", ButtonNode);
+			//***Read State and Data***
+			var fd_in;  //filedescripter InputFile
+			try {
+				fd_in = fs.openSync('/tmp/i2c_10_in', 'r');
+				var contents = fs.readFileSync(fd_in, 'utf8');
+				if (contents == "<offline>") {
+					node.status({ fill: "red", shape: "dot", text: "disconnected" });
+				} else if (contents == "<online>") {
+					node.status({ fill: "green", shape: "dot", text: "connected" });
+				} else {
+					node.status({ fill: "green", shape: "dot", text: "connected" });
+					msg.payload = contents;
+					node.send(msg);
+				}
+			} catch (error) {
+				node.status({ fill: "red", shape: "dot", text: "disconnected" });
+				node.error(error);
+			} finally {
+				if(fd_in){
+					fs.closeSync(fd_in);
+				}
+			}
+		});
+	}
+	RED.nodes.registerType("button", ButtonNode);
 }
